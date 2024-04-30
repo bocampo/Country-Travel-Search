a18e506a550702be13c3ca2c6e778f12414e0945
Bryan O <bocampo91@yahoo.com>
2024-04-30 12:57:40 -0400

    Created seeds file for database

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO country_visa( country_name, visa)
+VALUES (1, 'China', 'Visa Required'),
+       (2, 'Thailand', 'Visa Required'),
+       (3, 'Italy', 'Not required for less than 90 days'),
+       (4, 'France', 'Not required for less than 90 days'),
+       (5, 'Spain', 'Not required for less than 90 days'),
+       (6, 'Germany', 'Not required for less than 90 days'),
+       (7, 'Turkey', 'NR for less than 90 days'),
+       (8, 'Mexico', 'NR for less than 180 days'),
+       (9, 'United Kingdom', 'NR for less than 180 days'),
+       (10, 'Greece', 'NR for less than 90 days');
