95860e49375ce081e6d9413613013e3e6887943e
Bryan O <bocampo91@yahoo.com>
2024-05-01 14:34:10 -0400

    started chaning saved.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
     const countries = savedData.map((countries) => countries.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', {
+    res.render('saved', {
       countries,
       logged_in: req.session.logged_in
     });
@@ -71,7 +71,7 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  
+
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
